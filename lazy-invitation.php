cdfbaa9d6d9bdea41f6962e049412f2b1b78f130
juliobox <juliobox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-24 23:29:37 +0000

    v 1.0
    git-svn-id: https://plugins.svn.wordpress.org/lazy-invitation/trunk@1206070 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lazy-invitation.php
@@ -0,0 +1,232 @@
+<?php
+/*
+Plugin Name: BAW Slack Lazy invitation
+Description: Slack Lazy Invitation lets you auto invite anyone on you Slack Group.
+Author: Julio Potier
+Author URI: http://boiteaweb.fr
+Plugin URI: http://boiteaweb.fr/?p=8611
+Version: 1.0
+Licence: GPLv2
+Domain: bawsi
+*/
+
+define( 'BAWSI_VERSION', '1.0' );
+
+/**
+ * Load plugin textdomain.
+ *
+ * @since 1.0
+ */
+add_action( 'plugins_loaded', 'bawsi_load_textdomain' );
+function bawsi_load_textdomain() {
+	load_plugin_textdomain( 'bawsi', false, dirname( plugin_basename( __FILE__ ) ) . '/langs' ); 
+}
+
+/*
+* Prints the form, do the request or print messages
+*
+* @since 1.0
+*/
+add_action( 'login_form_slack-invitation', 'bawsi_do_invit' );
+function bawsi_do_invit() {
+	$bawsi_options = get_site_option( 'bawsi' );
+	$message = '';
+	if ( ! isset( $bawsi_options['groupname'] ) || false == trim( $bawsi_options['groupname'] ) ) {
+		$message .= '<p class="message">' . __( '<b>ERROR</b> GroupName is not set!', 'bawsi' ) . '</p><br>';
+		$response = true;
+	}
+	if ( ! isset( $bawsi_options['token'] ) || false == trim( $bawsi_options['token'] ) ) {
+		$message .= '<p class="message">' . __( '<b>ERROR</b> Token is not set!', 'bawsi' ) . '</p>';
+		$response = true;
+	}
+	if ( empty( $message ) ) {
+		$message = '<p class="message">';
+		$message .= sprintf( __( 'Enter your email address to join the <a href="http://%1$s.slack.com"><b>%1$s</b></a> group on Slack.', 'bawsi' ), $bawsi_options['groupname'] );
+		$message .= '</p>';
+	}
+	if ( ! empty( $_POST['email'] ) && is_email( $_POST['email'] ) ) {
+		$data = array( 
+			'email' => $_POST['email'], 
+			'channels' => '',
+			'first_name' => '', 
+			'token' => $bawsi_options['token'], 
+			'set_active' => 'true',
+			'_attempts' => '1',
+		);
+		$slack_url = esc_url( 'https://' . $bawsi_options['groupname'] .'.slack.com' );
+		$dom = '<p class="message" style="min-height:64px"><img src="https://slack-assets2.s3-us-west-2.amazonaws.com/10068/img/slackbot_192.png" style="float:left;height:64px;width=64px" heigt="64" width="64"> ';
+		$response = wp_remote_post( $slack_url . '/api/users.admin.invite?t=1', array( 'body' => $data ) );
+		if( ! is_wp_error( $response ) && 200 === wp_remote_retrieve_response_code( $response ) ) {
+			$return = json_decode( wp_remote_retrieve_body( $response ) );
+			if ( $return->ok ) {
+				$message = $dom . sprintf( __( 'Invitation launched, thank you.<br>See you soon on <a href="%1$s">%2$s</a>.<br><i>- Slackbot</i>', 'bawsi' ), $slack_url, esc_html( $bawsi_options['groupname'] ) );
+			} 
+			if ( isset( $return->error ) ) {
+				switch( $return->error ) {
+					case 'already_in_team' :
+						$message = $dom . __( 'You are already in this team!', 'bawsi' );
+					break;
+					case 'already_invited' :
+					case 'sent_recently' :
+						$message = $dom . __( 'You have already been invited in this team!', 'bawsi' );
+					break;
+					default:
+						$message = $dom . _( 'Unknow error.', 'bawsi' );
+					break;
+				}
+			}
+		} else {
+			$message = $dom . _( 'Unknow error.', 'bawsi' );
+		}
+		$message .= '</p>';
+	}
+
+	login_header( __( 'SlackBot Invitation', 'bawsi' ), $message );
+	if ( ! isset( $response ) ) {
+	?>
+		<style>
+		.message{}
+		</style>
+		<form action="" method="post">
+			<p><?php _e( 'Email: ', 'bawsi' ); ?><input type="text" name="email" value="" title="<?php esc_attr_e( 'Email address', 'bawsi' ); ?>" /></p>
+			<?php do_action( 'slack-invitation-before-submit' ); ?>
+			<p><input type="submit" value="<?php esc_attr_e( 'Get an invitation', 'bawsi' ); ?>" name="submit" id="submit" class="button button-primary"/></p>
+		</form>
+	<?php
+	} elseif ( true !== $response ) {
+	?>
+		<p id="backtoblog"><a href="<?php echo site_url( 'wp-login.php?action=slack-invitation' ); ?>"><?php _e( 'Ask for another invitation?', 'bawsi' ); ?></a></p>
+	<?php 
+	}
+	login_footer( 'email' );
+	die();
+}
+
+/*
+* Add the default option
+*
+* @since 1.0
+*/
+register_activation_hook( __FILE__, 'bawsi_activation' );
+function bawsi_activation() {
+	if ( ! get_option( 'bawsi' ) ) {
+		add_option( 'bawsi', 
+			array(	'groupname' => false, 
+					'token' => false,
+				), 
+		'', 'no' );
+	}
+}
+
+/*
+* Remove the option
+*
+* @since 1.0
+*/
+register_uninstall_hook( __FILE__, 'bawsi_uninstall' );
+function bawsi_uninstall() {
+	delete_option( 'bawsi' );
+}
+
+/*
+* Add the menu
+*
+* @since 1.0
+*/
+add_action( 'admin_menu', 'bawsi_setting_menu' );
+function bawsi_setting_menu() {
+	add_options_page( 'Slack Invit', 'Slack Invit', 'manage_options', 'slackinvit_settings', 'slackinvit_settings_page' );
+	register_setting( 'bawsi_settings', 'bawsi' );
+}
+
+/*
+* Settings page callback
+*
+* @since 1.0
+*/
+function slackinvit_settings_page() {
+	add_settings_section( 'bawsi_settings_page', __( 'General', 'bawsi' ), 'bawsi_info', 'bawsi_settings' );
+		add_settings_field( 'bawsi_field_groupname', __( 'GroupName?', 'bawsi' ), 'bawsi_field_groupname', 'bawsi_settings', 'bawsi_settings_page' );
+		add_settings_field( 'bawsi_field_token', __( 'Token?', 'bawsi' ), 'bawsi_field_token', 'bawsi_settings', 'bawsi_settings_page' );
+?>
+	<div class="wrap">
+		<h2>Slack Lazy Invitation <small>v<?php echo BAWSI_VERSION; ?></small></h2>
+
+		<form action="options.php" method="post">
+			<?php settings_fields( 'bawsi_settings' ); ?>
+			<?php do_settings_sections( 'bawsi_settings' ); ?>
+			<?php submit_button(); ?>
+		</form>
+	</div>
+<?php
+}
+
+/*
+* Settings section callback
+*
+* @since 1.0
+*/
+function bawsi_info() {
+	_e( 'Remember that your username will be used for the invitation mail.', 'bawsi' ); 
+}
+
+/*
+* Print the groupname input
+*
+* @since 1.0
+*/
+function bawsi_field_groupname() {
+	$bawsi_options = get_option( 'bawsi' );
+	?>
+	<label><input type="text" required="required" name="bawsi[groupname]" value="<?php echo esc_attr( $bawsi_options['groupname'] ); ?>"></label>
+	<p class="description">
+	<?php _e( 'This is the name of your slack group.', 'bawsi' ); ?>
+	</p>
+	<?php
+}
+
+/*
+* Print the token input + bookmarklet
+*
+* @since 1.0
+*/
+function bawsi_field_token() {
+	$bawsi_options = get_option( 'bawsi' );
+	?>
+	<label><input type="text" required="required" name="bawsi[token]" value="<?php echo esc_attr( $bawsi_options['token'] ); ?>" size="50"></label>
+	<p class="description">
+	<?php _e( 'This is the security token of your slack invitation group.', 'bawsi' ); ?>
+	</p>
+	<br>
+	<p class="description">
+		<?php _e( 'To find your token you have to use the bookmarklet below (drag/drop in your brower toolbar) on your invitation page like <code>https://YOURGROUP.slack.com/admin/invites</code>', 'bawsi' ); ?>
+	</p>
+	<p><a class="button button-small button-secondary" href="javascript:prompt('Slack Invit API Token', boot_data.api_token);" onclick="alert('<?php echo esc_js( __( 'Drag/drop me in our browser toolbar before!', 'bawsi' ) ); ?>');return false;"><?php _e( 'SlackInvit Token Api', 'bawsi' ); ?></a></p>
+	<p class="description">
+		<?php _e( 'Why do i have to do that?', 'bawsi' ); ?><br>
+		<?php _e( 'Because this is the only available token to auto invite people on your Slack.', 'bawsi' ); ?>
+	</p>
+<?php
+}
+
+/*
+* Add the settings links on plugins page
+*
+* @since 1.0
+*/
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'bawsi_settings_action_links' );
+function bawsi_settings_action_links( $links ) {
+	array_unshift( $links, '<a href="' . admin_url( 'options-general.php?page=slackinvit_settings' ) . '">' . __( 'Settings' ) . '</a>' );
+	return $links;
+}
+
+/*
+* Add the slug into sf move login
+*
+* @since 1.0
+*/
+add_filter( 'sfml_additional_slugs', 'bawsi_slackinvit_slug' );
+function bawsi_slackinvit_slug( $slugs ) {
+	$slugs['slack-invitation'] = 'SlackInvit';
+	return $slugs;
+}
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Slack Lazy Invitation ===
+Contributors: juliobox
+Tags: slack
+Requires at least: 4.0
+Tested up to: 4.3
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Slack Lazy Invitation lets you auto invite anyone on you Slack Group.
+
+== Description ==
+
+Slack Lazy Invitation lets you auto invite anyone on you Slack Group without adding each member each time they want to be invited. Boring.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload the zip content into the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+1. Why do i have to use the bookmarklet to find this api key?
+
+Be cause there is no other way to get it, sorry, this is the trick ;)
+
+== Screenshots ==
+
+1. Invitation Screen
+1. Invitation Sent
+1. Settings
+
+== Changelog ==
+
+= 1.0 =
+* 25 july 2015
+* First Release
